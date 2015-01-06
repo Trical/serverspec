9dc547deb02da68ede042246531164e2be9503e3
Tim <timderyck@europe.com>
2015-01-06 19:15:44 +0100

    remove old test

--- spec/common/common_spec.rb
+++ spec/common/common_spec.rb
@@ -23,22 +23,4 @@ packages = [
 
     end
 
-    describe "The network should be configured properly" do
-
-      property[:networks].each do |p|
-        describe command("cat /sys/class/net/eth#{p['device_id']}/address") do
-          its(:stdout) {should include "#{p['mac_address'].downcase}" }
-        end
-
-        describe interface("eth#{p['device_id']}") do
-          #its(:speed) { should eq 1000 }
-          it { should have_ipv4_address("#{p['ip_address']}/24") }
-        end
-      end
-
-      describe default_gateway do
-        its(:ipaddress) { should eq "#{property[:default_gateway]}" }
-      end
-
-    end #Network
 end
